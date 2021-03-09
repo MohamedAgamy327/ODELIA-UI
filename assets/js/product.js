4969c6ccab20aaf2e51a5503747674721e310c4e
Mohamed Agamy <mohamedagamy327@gmail.com>
2021-03-09 17:13:19 +0200

    change red color to greed

--- assets/js/product.js
+++ assets/js/product.js
@@ -2,7 +2,7 @@
 const card = document.querySelector(".card");
 const container = document.querySelector(".container2");
 //Items
-const title = document.querySelector(".title");
+const title = document.querySelector(".title-block");
 const sneaker = document.querySelector(".sneaker img");
 const purchase = document.querySelector(".purchase");
 const description = document.querySelector(".info h3");
