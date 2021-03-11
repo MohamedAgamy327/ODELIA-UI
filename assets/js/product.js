14a17696115f00e2ea44862fafd9d22295a1b580
Mohamed Agamy <mohamedagamy327@gmail.com>
2021-03-11 14:37:43 +0200

    test mobile

--- assets/js/product.js
+++ assets/js/product.js
@@ -9,46 +9,51 @@ function movement(x) {
   const sneaker = document.querySelector("." + x + " .sneaker img");
   const description = document.querySelector("." + x + " .info p");
 
-  //Moving Animation Event
-  container.addEventListener("mousemove", (e) => {
-    let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-    let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-  });
-  //Animate In
-  container.addEventListener("mouseenter", (e) => {
-    card.style.transition = "none";
-    //Popout
-    title.style.transform = "translateZ(150px)";
-    sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
-    description.style.transform = "translateZ(125px)";
-  });
-  //Animate Out
-  container.addEventListener("mouseleave", (e) => {
-    card.style.transition = "all 0.5s ease";
-    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-    //Popback
-    title.style.transform = "translateZ(0px)";
-    sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
-    description.style.transform = "translateZ(0px)";
-  });
+
+  if (/Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    container.addEventListener("pointerenter", (e) => {
+      card.style.transition = "none";
+      //Popout
+      title.style.transform = "translateZ(150px)";
+      sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+      description.style.transform = "translateZ(125px)";
+    });
+
+    container.addEventListener("pointerleave", (e) => {
+      card.style.transition = "all 0.5s ease";
+      card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+      //Popback
+      title.style.transform = "translateZ(0px)";
+      sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+      description.style.transform = "translateZ(0px)";
+    });
+  }
+
+  else {
+    //Moving Animation Event
+    container.addEventListener("mousemove", (e) => {
+      let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+      let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+      card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+    });
+    //Animate In
+    container.addEventListener("mouseenter", (e) => {
+      card.style.transition = "none";
+      //Popout
+      title.style.transform = "translateZ(150px)";
+      sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+      description.style.transform = "translateZ(125px)";
+    });
+    //Animate Out
+    container.addEventListener("mouseleave", (e) => {
+      card.style.transition = "all 0.5s ease";
+      card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+      //Popback
+      title.style.transform = "translateZ(0px)";
+      sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+      description.style.transform = "translateZ(0px)";
+    });
+  }
 }
 
 
-
-container.addEventListener("pointerenter", (e) => {
-  card.style.transition = "none";
-  //Popout
-  title.style.transform = "translateZ(150px)";
-  sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
-  description.style.transform = "translateZ(125px)";
-});
-
-container.addEventListener("pointerleave", (e) => {
-  card.style.transition = "all 0.5s ease";
-  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-  //Popback
-  title.style.transform = "translateZ(0px)";
-  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
-  description.style.transform = "translateZ(0px)";
-})
