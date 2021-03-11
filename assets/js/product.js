786e3bc5cb4008516155d6668a942345893d767f
Mohamed Agamy <mohamedagamy327@gmail.com>
2021-03-11 12:46:56 +0200

    mobile move

--- assets/js/product.js
+++ assets/js/product.js
@@ -35,37 +35,20 @@ function movement(x) {
 }
 
 
-// //Movement Animation to happen
-// const container = document.querySelector(".container2");
 
-// const card = document.querySelector(".card");
-// //Items
-// const title = document.querySelector(".title-block");
-// const sneaker = document.querySelector(".sneaker img");
-// const description = document.querySelector(".info p");
+container.addEventListener("pointerenter", (e) => {
+  card.style.transition = "none";
+  //Popout
+  title.style.transform = "translateZ(150px)";
+  sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+  description.style.transform = "translateZ(125px)";
+});
 
-// //Moving Animation Event
-// container.addEventListener("mousemove", (e) => {
-//   let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-//   let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-//   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-// });
-// //Animate In
-// container.addEventListener("mouseenter", (e) => {
-//   card.style.transition = "none";
-//   //Popout
-//   title.style.transform = "translateZ(150px)";
-//   sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
-//   description.style.transform = "translateZ(125px)";
-// });
-// //Animate Out
-// container.addEventListener("mouseleave", (e) => {
-//   card.style.transition = "all 0.5s ease";
-//   card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-//   //Popback
-//   title.style.transform = "translateZ(0px)";
-//   sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
-//   description.style.transform = "translateZ(0px)";
-// });
-
-
+container.addEventListener("pointerleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  //Popback
+  title.style.transform = "translateZ(0px)";
+  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+  description.style.transform = "translateZ(0px)";
+})
